846811a3a61eaafeec0009a92c58ad6d9e99b37e
Lucas Oliveira <lucasolidev@gmail.com>
2023-01-26 19:28:17 -0300

    Revert "Delete"
    This reverts commit cbca472a37c8001044d8aff09a4b0a3e3f215507.

---
+++ Calculadora/calculator_class.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+    from typing import List
+
+class Calculator:
+    def __init___(
+        self,
+        root: tk.Tk,
+        label: tk.Label,
+        display: tk.Entry,
+        buttons: List[List[tk.Button]]
+    ):
+        self.root = root
+        self.label = label
+        self.display = display
+        self.buttons = buttons
+
+
